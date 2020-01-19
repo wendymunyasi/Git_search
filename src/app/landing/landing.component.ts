233b41841d34ff486fdb470063799feadb750e26
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-19 09:29:10 +0300

    Create a variable private user of type any and used it in the constructor in landing.component.ts.

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -8,11 +8,15 @@ import { UserService } from './../user-service/user.service';
 })
 export class LandingComponent implements OnInit {
 
+  private user: any;
+
   constructor(private userService: UserService) {
     this.userService.getProfileInfo().subscribe(user => {
       console.log(user);
+      this.user = user;
+
     });
-   }
+  }
 
   ngOnInit() {
   }
