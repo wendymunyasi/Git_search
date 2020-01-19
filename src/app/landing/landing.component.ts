f76b058209ec1461d545f73116a77ebe32f69fd1
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-19 08:02:35 +0300

    In landing.component.ts, import UserService and define the constructor function.

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './../user-service/user.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) {
+    this.userService.getProfileInfo().subscribe(user => {
+      console.log(user);
+    });
+   }
 
   ngOnInit() {
   }
