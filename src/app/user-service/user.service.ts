43b9bd5ca809a57c7669b192f77186234135bae8
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-19 08:03:41 +0300

    Import HttpClient, environment, map, define the constructor function and getProfileInfofunction and many more.

--- src/app/user-service/user.service.ts
+++ src/app/user-service/user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private username: string;
+  private clientid = '471dc5a63a45103713a4';
+  private clientsecret = 'e6f34df231761054ce6976a22d07434a1b64d342';
+
+  constructor(private http: HttpClient) {
+    console.log("service is running");
+    this.username = 'wendymunyasi';
+
+  }
+
+  getProfileInfo() {
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+      // .pipe(map((res: Response)=>res.json()));
+  }
 }
