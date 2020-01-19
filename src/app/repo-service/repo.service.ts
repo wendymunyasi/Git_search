a156287c6d62887221badcced73cd89036d3e20b
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-19 16:57:26 +0300

    Pass values into the constructor and define the getUserRepos function in repo.service.ts.

--- src/app/repo-service/repo.service.ts
+++ src/app/repo-service/repo.service.ts
@@ -12,5 +12,14 @@ export class RepoService {
   private clientid = '471dc5a63a45103713a4';
   private clientsecret = 'e6f34df231761054ce6976a22d07434a1b64d342';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    console.log("repo service is running");
+    this.username = 'wendymunyasi';
+
+  }
+
+  getUserRepos() {
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    // .pipe(map((res: Response)=>res.json()));
+  }
 }
