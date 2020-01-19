46de82020b7b131d2c42d386fc99046da83e9c56
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-19 16:59:10 +0300

    Import RepoService, create a repos variable of type any, add RepoService to the constructor and define thw function of fetching repos data from GitHub. In landing.component.ts.

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './../user-service/user.service';
+import { RepoService } from './../repo-service/repo.service';
 
 @Component({
   selector: 'app-landing',
@@ -9,11 +10,18 @@ import { UserService } from './../user-service/user.service';
 export class LandingComponent implements OnInit {
 
   private user: any;
+  private repos: any;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private repoService: RepoService) {
     this.userService.getProfileInfo().subscribe(user => {
       console.log(user);
       this.user = user;
+
+    });
+
+    this.repoService.getUserRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
 
     });
   }
