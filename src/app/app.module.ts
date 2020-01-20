a6321ea55ad7648b1373b09ac192c48b7c2dfb35
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-20 08:23:20 +0300

    Remove GithubFormComponent from imports and declarations in app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { GithubFormComponent } from './github-form/github-form.component';
 import { LandingComponent } from './landing/landing.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -20,7 +19,6 @@ import { GithubComponent } from './github/github.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    GithubFormComponent,
     LandingComponent,
     AboutComponent,
     NotFoundComponent,
