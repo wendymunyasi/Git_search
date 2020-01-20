a1270d4e61dad01437282635375fee5d4f2a6eaf
Wendy Munyasi <wendymunyasi@gmail.com>
2020-01-20 04:40:24 +0300

    Import landingComponent and NotFoundComponent and declared their paths under routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { LandingComponent } from './landing/landing.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,6 +7,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent},
+  { path: 'landing', component: LandingComponent},
+
+
+  { 
+    path: "", redirectTo: "/landing", pathMatch: "full" },
+  { path: '**', component: NotFoundComponent},
+
 ];
 
 @NgModule({
