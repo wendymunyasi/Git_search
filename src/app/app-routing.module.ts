dfdf59e10712e779f8deecce2441704e1e7facc7
wendymunyasi <wendymunyasi@gmail.com>
2020-09-07 18:25:40 +0300

    Fix routing in app-routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,17 @@
-import { GithubComponent } from './github/github.component';
 import { LandingComponent } from './landing/landing.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundComponent } from './not-found/not-found.component'; 
 import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'about', component: AboutComponent},
   { path: 'landing', component: LandingComponent},
-  { path: 'github', component: GithubComponent},
+  { path: 'about', component: AboutComponent},
 
 
-  // { path: "", redirectTo: "landing", pathMatch: "full" },
-  // { path: '**', component: NotFoundComponent},
+  { path: "", redirectTo: "landing", pathMatch: "full" },
+  { path: '**', component: NotFoundComponent},
 
 ];
 
